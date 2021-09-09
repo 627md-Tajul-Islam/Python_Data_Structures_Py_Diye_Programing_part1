0bb5c7f03011c56e3d897eecfdb52eb486423a2f
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-09 01:19:41 -0700

    List method added

--- List Methods.py
+++ List Methods.py
@@ -1,4 +1,4 @@
 saarc = ["Bangladesh", "India", "Srilanka","Bhutan", "Pakistan", "Nepal"]
 print(saarc)
 saarc = saarc.append("Afghanistan")
-print(saarc)
+print(saarc) # append method to put something
