5f6c5a841c33845e8e6229f0f26603d8ace72c1e
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-09 01:43:18 -0700

    List method added

--- List Methods.py
+++ List Methods.py
@@ -4,8 +4,13 @@ saarc.append("Afghanistan")
 print(saarc) # append method to put something
 
 saarc.sort()
-print(saarc) # ranked from 1st to last
+print(saarc)  # ranked from 1st to last
 
 list = [1,3,5,5,6,4,3,1]
 list.sort()
-print(list)
+print(list) # ranked from 1st to last
+
+# off topic
+a=[1,2,3,4,5,6,7,8]
+b=a+[9,10,11,12]
+print(b) # adding 2 lists
