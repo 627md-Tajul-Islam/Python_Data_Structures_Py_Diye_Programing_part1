b98fe9be0355225492b6d0ece61f5827bbc79def
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-09 01:41:24 -0700

    List method added

--- List Methods.py
+++ List Methods.py
@@ -4,4 +4,8 @@ saarc.append("Afghanistan")
 print(saarc) # append method to put something
 
 saarc.sort()
-print(saarc)
+print(saarc) # ranked from 1st to last
+
+list = [1,3,5,5,6,4,3,1]
+list.sort()
+print(list)
