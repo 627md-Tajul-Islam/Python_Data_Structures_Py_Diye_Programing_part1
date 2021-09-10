cb9cdb97823b2e705cf50fc6f01fd7235916fa02
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-09 23:00:11 -0700

    List comprehensions added

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -8,3 +8,9 @@ print(new_li)
 new_li = [2* x for x in li ]
 print(new_li)
 
+li = [1,2,3,4,5,6,7,8,9,10]
+even_number = []
+for x in li:
+    if x % 2 == 0:
+        even_number.append(x)
+print(even_number)
