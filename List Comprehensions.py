d190755d9643518b069e13c22c80551ab380def4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-09 22:32:13 -0700

    List comprehensions added

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -3,3 +3,8 @@ new_li = []
 for x in li:
     new_li.append(2 * x) # everyone is double to their previous books
 print(new_li)
+
+# using list comprehension
+new_li = [2* x for x in li ]
+print(new_li)
+
