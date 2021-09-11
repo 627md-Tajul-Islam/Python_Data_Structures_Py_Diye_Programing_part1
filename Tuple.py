c4e583935e83beb6c3acbae860bdd495c0f61d9a
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-10 23:52:50 -0700

    tuple added

--- Tuple.py
+++ Tuple.py
@@ -29,3 +29,7 @@ tuple = (1,2,3,4,5)
 print(tuple)
 #tuple[0] = 100 tuple cant change
 #print(tuple) 'tuple' object does not support item assignment
+
+nums = (1,2,3,4)
+n1,n2,n3,n4 = nums
+print(n1)
