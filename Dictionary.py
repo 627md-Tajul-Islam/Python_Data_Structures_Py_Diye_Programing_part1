1fee4f057cfade89ed63e0fe44573d8af4503ac4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-12 19:20:22 -0700

    Dictionary added

--- Dictionary.py
+++ Dictionary.py
@@ -5,3 +5,5 @@ print("Your mark is", Marks[int(Roll)-1])
 
 # this is for double subject
 Mark = [[90,60],[80,70],[50,50],[70,50],[40,60]]
+Rolls = input("Please enter your roll number: ")
+print("Your mark is", Mark[int(Rolls)-1])
