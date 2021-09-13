7467d94dbcb0de6aff0ee3e81cbea1d5cfe9dd49
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-12 19:28:13 -0700

    Dictionary added

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,4 @@
+"""
 # this was for 1 number
 Marks = [80,90,67,84,65]
 Roll = input("Please enter your roll number: ")
@@ -7,7 +8,9 @@ print("Your mark is", Marks[int(Roll)-1])
 Mark = [[90,60],[80,70],[50,50],[70,50],[40,60]]
 Rolls = input("Please enter your roll number: ")
 print("Your mark of English & Bangla is :", Mark[int(Rolls)-1])
+"""
 
+# dictionary
 Marks = {
     1:77,
     2:76,
