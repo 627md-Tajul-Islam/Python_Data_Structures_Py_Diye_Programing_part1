4c666f36442a3688dab78e2c7fec191fbb58c50b
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-13 20:06:37 -0700

    Dictionary added

--- BD_Division.py
+++ BD_Division.py
@@ -6,3 +6,15 @@ bd_division_info["Barisal"] = {
     "upzilla": 39,
     "union":333
 }
+#2
+bd_division_info["Khulna"] = {
+    "district": 10,
+    "upzilla": 59,
+    "union":270
+}
+#3
+bd_division_info["Dhaka"] = {
+    "district": 10,
+    "upzilla": 59,
+    "union":1833
+}
