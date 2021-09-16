30dc689b303e2d20ba253607e45c7dd9179a8473
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-15 19:28:59 -0700

    dots

--- Some_Funny_Programs/Random turtle dots.py
+++ Some_Funny_Programs/Random turtle dots.py
@@ -6,7 +6,7 @@ turtle.penup()
 for i in range(100):
     x = random.randint(-200, 200)
     y = random.randint(-200, 200)
-    turtle.set_position(x,y)
+    turtle.setposition(x,y)
     turtle.dot()
 
 turtle.done()
