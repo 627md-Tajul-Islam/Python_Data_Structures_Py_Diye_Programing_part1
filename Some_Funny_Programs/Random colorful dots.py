f281f75992b5102254d35e639eb6a4f3812f4076
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-15 19:54:06 -0700

    dots

--- Some_Funny_Programs/Random colorful dots.py
+++ Some_Funny_Programs/Random colorful dots.py
@@ -5,7 +5,6 @@ import random
 colors = ["red", "green", "blue", "yellow", "orange", "black", "purple"]
 
 turtle.penup()
-
 for i in range(100):
     x = random.randint(-100,100)
     y = random.randint(-100,100)
