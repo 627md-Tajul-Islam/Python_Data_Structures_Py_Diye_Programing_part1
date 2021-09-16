6beb3da0a02b4f2d35494e2596222357c52790be
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-15 19:51:12 -0700

    dots

--- Some_Funny_Programs/Random colorful dots.py
+++ Some_Funny_Programs/Random colorful dots.py
@@ -10,3 +10,7 @@ for i in range(100):
     x = random.randint(100,100)
     y = random.randint(100,100)
 
+# set a random position
+    turtle.set_position(x,y)
+# set a random color
+    i = random.randint(0, len(colors)-1)
