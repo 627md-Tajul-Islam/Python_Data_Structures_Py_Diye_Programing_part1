922c1a1ef1f3eaeda4a5245c338c25b5866fc4fa
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-17 02:32:17 -0700

    prime number

--- Some_Funny_Programs/Prime_Numbers2.py
+++ Some_Funny_Programs/Prime_Numbers2.py
@@ -1,3 +1,9 @@
 def is_prime1(n):
     if n < 2:
         return False
+    prime = True
+    for x in range(2,n):
+        if n % x == 0:
+            print(n, "is divisible by", x)
+            prime = False
+    return prime
