538e0d92dd225d0609508289b587ea10d70b373a
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-17 02:10:51 -0700

    prime number

--- Some_Funny_Programs/Prime_Numbers.py
+++ Some_Funny_Programs/Prime_Numbers.py
@@ -6,4 +6,8 @@ def is_prime1(n):
         if n % x == 0:
             print(n, "is divisible by", x)
             prime = False
-    return prime
+    return
+
+while True:
+    number = input("Please enter a number (press 0 to exit): ")
+    number = int(number)
