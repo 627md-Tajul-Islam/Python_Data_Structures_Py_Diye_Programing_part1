9f1a62fc7142d092d68541479100061f1892a2ae
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-17 00:49:24 -0700

    f number

--- Some_Funny_Programs/ProgramPlayingWithProgram.py
+++ Some_Funny_Programs/ProgramPlayingWithProgram.py
@@ -1,13 +1,17 @@
 import random
 number = random.randint(1,1000)
 attempts = 0
+
 low = 1
 high = 1000
+
 while True:
     print("Guess between 1-1000: ")
     input_number = (low + high) // 2 # only integer version
+
     print("My guess is", input_number)
     attempts += 1
+
     if input_number == number:
         print("Its Correct")
         break
