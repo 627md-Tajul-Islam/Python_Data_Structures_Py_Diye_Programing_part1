65d26a0911c533899fbd5cd7ac11673e744094d4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-17 19:45:07 -0700

    prime number

--- Some_Funny_Programs/Prime_Number3.py
+++ Some_Funny_Programs/Prime_Number3.py
@@ -4,3 +4,6 @@ def is_prime3(n):
     if n % 2 == 0:
         print(n, "is divisible by 2")
         return False # all even number except 2 are not prime
+
+    if n < 2:
+        return False # numbers less than 2 are not prime
