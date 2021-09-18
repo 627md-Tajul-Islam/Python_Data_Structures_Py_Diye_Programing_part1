7a278e84698c1c5d3b06d8f28858dd59fd2476ef
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-17 23:36:27 -0700

    prime number

--- Some_Funny_Programs/Final_Code.py
+++ Some_Funny_Programs/Final_Code.py
@@ -18,3 +18,21 @@ def is_prime3(n = 1013):
         return True # 2 is prime
     if n % 2 == 0:
         return False # numbers less than 2 are not prime
+    prime = True
+    for x in range(3,n,2):
+        if n % x == 0:
+            prime = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
